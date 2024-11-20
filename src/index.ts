3f5d09ee98b4bfdec74b4ae073919e21353a11d3
CodeRunner-003 <freshprojects245@gmail.com>
2024-11-20 10:50:21 +0530

    Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
-import express from 'express';
+import  express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import connectDB from './config/db';
+
+
+dotenv.config();
 
 const app = express();
 
-// Connect to MongoDB
-connectDB();
-
-// Middleware
+app.use(cors());
 app.use(express.json());
 
-// Routes
+const PORT = process.env.PORT || 5000;
+
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-// Start server
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+connectDB();
